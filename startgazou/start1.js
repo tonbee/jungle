74b51cb37ed94c0c008b6c980255c7647b340d40
tonbee <stylohouse@yahoo.co.jp>
2014-06-08 16:30:48 +0900

    インデクスページをリネーム

---
+++ startgazou/start1.js
@@ -0,0 +1,38 @@
+﻿
+
+function test2()
+{
+
+
+var ele = document.getElementById("sbutton");
+
+
+ele.src="./startgazou/startbutton.png" ;
+
+ele.style.position = 'absolute'; 
+ele.style.left = '210px'; 
+ele.style.top  = '261px'; 
+
+
+}//function test1()
+
+
+
+function startido()
+{
+
+location.href="mori.html";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
